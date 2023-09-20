0d31a2a05cae750f716e2d5698cad571d20d7e8e
Sagar Chowdhury <sagarchowdhury4693@gmail.com>
2023-09-21 00:32:29 +0530

    Upadte Functionality Done

--- src/main/java/dev/SagarChowdhury/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/SagarChowdhury/contentcalendar/controller/ContentController.java
@@ -8,42 +8,45 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
 
-    private  final ContentCollectionRepository repository;
+    private final ContentCollectionRepository repository;
 
     @Autowired
-    public ContentController(ContentCollectionRepository repository)
-    {
-        this.repository=repository;
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
     }
 
     @GetMapping("")
-    public List<Content> findAll()
-    {
+    public List<Content> findAll() {
         return repository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Content findById(@PathVariable Integer id){
+    public Content findById(@PathVariable Integer id) {
         return repository.findById(id).orElseThrow(
-                ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Content not Found "));
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not Found "));
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content)
-    {
-       repository.save(content);
+    public void create(@RequestBody Content content) {
+        repository.save(content);
     }
 
+     @PutMapping("/{id}")
+     public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not Found ");
 
-
-
+        }
+        repository.save(content);
+     }
 
 
 }
